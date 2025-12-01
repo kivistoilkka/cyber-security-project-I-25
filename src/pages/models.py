9af9ab0273d5093481fae993058bb951129140a5
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2025-12-02 01:35:20 +0200

    Add first db table, remove sd.sqlite3 from repository

--- src/pages/models.py
+++ src/pages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    note_text = models.CharField(max_length=200)
+    save_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.note_text
