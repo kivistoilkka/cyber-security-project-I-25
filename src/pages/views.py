d12d5dd6612f6b533e6df30558a6a0226f8847de
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2025-12-04 00:10:26 +0200

    Add templates for index and notes

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,6 +1,17 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Note
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the pages index.")
+    note_list = Note.objects.order_by("-save_date")
+    context = {"note_list": note_list}
+    return render(request, "pages/index.html", context)
+
+def note(request, note_id):
+    note = get_object_or_404(Note, pk=note_id)
+    return render(
+        request,
+        "pages/note.html",
+        {"note": note}
+    )
